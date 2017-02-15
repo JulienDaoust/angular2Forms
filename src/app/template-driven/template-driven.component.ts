4e7e18fe3672703de1fb290a5cd87c21f0d9f962
julienDaoustCistec <jdaoust@cistec.ca>
2017-02-15 15:01:48 -0500

    two-way data binding template driven form

--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'template-driven',
-  templateUrl: 'template-driven.component.html'
+  templateUrl: 'template-driven.component.html',
+  styles: [`
+    input.ng-invalid {  
+    border: 1px solid red;  
+    }  
+  `]
 })
 export class TemplateDrivenComponent {
+
+  user = {
+    username: '',
+    email: '',
+    password: ''
+  };
+
+  onSubmit(form: NgForm) {
+    console.log(this.user)
+  }
 }
